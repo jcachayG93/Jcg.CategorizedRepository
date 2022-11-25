a9579f71bdc02e3b8577f0c09f68342903c05543
Julio Cachay <jcachay@mktg33.com>
2022-11-24 20:43:45 -0500

    cp

--- scripts/push.ps1
+++ scripts/push.ps1
@@ -1,5 +1,5 @@
 ﻿Get-ChildItem bin/Debug -Filter "*.nupkg" | ForEach-Object {
         Write-Host "push.ps1: Pushing $($_.Name)"
-        dotnet nuget push $_ --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
+      #  dotnet nuget push $_ --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
  
     }
